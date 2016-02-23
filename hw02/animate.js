78e3437360fd91d176d4d7d1d538b3b68e37d791
Yicheng Wang <alex.wyc2098@protonmail.ch>
2016-02-22 22:45:26 -0500

    done

--- hw02/animate.js
+++ hw02/animate.js
@@ -2,21 +2,31 @@ console.log("animate.js loaded");
 
 var c = document.getElementById('playground');
 var ctx = c.getContext('2d');
+var r = 0, d = 1;
 ctx.fillStyle = '#FF0000';
 
 // draws a circle, parameters are the context, center-x, center-y, and the
 // radius
 function draw_circle(ctx, x, y, r) {
+    ctx.beginPath();
     ctx.arc(x, y, r, 0, 2 * Math.PI);
     ctx.stroke();
+    ctx.fill();
 }
 
-c.addEventListener('click', function(e) {
-    draw_circle(ctx, e.offsetX, e.offsetY, 10);
-});
-
-document.getElementById('clear').addEventListener('click', function(e) {
-    e.preventDefault();
+var frame = function() {
+    console.log(r);
+    if (r == 0) {
+        d = 1;
+    }
+    if (r == 250) {
+        d = -1;
+    }
+    r = r + d;
     ctx.clearRect(0, 0, 500, 500);
-    ctx.beginPath();
-});
+    draw_circle(ctx, 250, 250, r);
+    window.requestAnimationFrame(frame);
+}
+
+document.getElementById('start').addEventListener('click', frame);
+
