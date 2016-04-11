bb538bdd9f9b17b6c7094609f5e29ea3250c69b2
Yicheng Wang <alex.wyc2098@protonmail.ch>
2016-04-11 12:00:30 -0400

    i dumb

--- hw09/hw09.py
+++ hw09/hw09.py
@@ -1,15 +1,15 @@
 from time import time
 
 def show_name(f):
-    def inner(*arg):
-        print f.func_name + '(' + str(*arg) + '): ' + str(f(*arg))
-        return f(*arg)
+    def inner(*args):
+        print f.func_name + '(' + str(*args) + '): ' + str(f(*args))
+        return f(*args)
     return inner
 
 def show_time(f):
-    def inner(*arg):
+    def inner(*args):
         init_t = time()
-        ret_val = f(*arg)
+        ret_val = f(*args)
         fin_t = time()
         print "Time: %f" % (fin_t - init_t)
         return ret_val
@@ -23,5 +23,16 @@ def fib(n):
     else:
         return fib(n-1) + fib(n-2)
 
-fib_log = fib(4)
-print fib_log
+@show_time
+#@show_name
+def quicksort(l1):
+    if len(l1) < 2:
+        return l1
+
+    return quicksort([x for x in l1[1:] if x < l1[0]]) + [l1[0]] + quicksort([x for x in l1[1:] if x >= l1[0]])
+
+qs = quicksort([1,21, 13,5,5,5,1,25,0])
+print qs
+
+#fib_log = fib(4)
+#print fib_log
